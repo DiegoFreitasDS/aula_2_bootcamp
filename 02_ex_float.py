fe3a8e565c09f3733acf5eef2ccf85217a27872b
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-03-28 20:36:07 -0300

    resolução2 type float

--- 02_ex_float.py
+++ 02_ex_float.py
@@ -1,13 +1,58 @@
 # Escreva um programa que receba dois números flutuantes e realize sua adição.
 
+# num1 = float(input("\nDigite um número qualquer: \n"))
+# num2 = float(input("\nDigite um segundo número qualquer: \n"))
+
+# soma = num1 + num2
+
+# print(f"\n A soma de {num1} com {num2} resulta o total de {soma:.2f}.")
+
+
 
 # Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+# num1 = float(input("\nDigite um número qualquer: \n"))
+# num2 = float(input("\nDigite um segundo número qualquer: \n"))
+
+# media = (num1 + num2)/2
+
+# print(f"\nA média entre {num1} e {num2} é igual à {media:.2f}")
+
 
 
 # Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 
+# print("\nEsse programa calcula a potência de um número, sendo que ambos os valores são digitados pelo usuário.\n")
+# print("Vamos começar\n")
+
+# num1 = float(input(f"Digite o número que você deseja saber: \n"))
+             
+# num2 = float(input("\nAgora digite o valor que você quer que o número seja elevado: \n"))
+
+# pot = num1**num2
+
+# print(f"\nO número {num1} elevado à potência de {num2} resulta em {pot:.2f}")
+
+
 
 # Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
+# print("\nEsse programa transforma os graus celsius digitados pelo usuario, para a temperatura em Fahrenheit.\n")
+# print("Vamos começar\n")
+
+# graus = float(input("Digite o valor em graus celsius: \n"))
+
+# celsius = (9/5)+32
+
+# convert = graus * celsius
+
+# print(f"\n{graus}º celsius em temperatura fahrenheit é {convert:.2f}.")
+
+
 
 # Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+# raio = float(input("\nDigite o raio do círculo: \n"))
+
+# area = 3.14159 * raio ** 2
+# print(f"\nA área do círculo é: {area:.2f}.")
