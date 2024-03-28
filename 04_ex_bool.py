a48e23a56719e2b20548bb29fac461b6d0375c79
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-03-28 20:39:26 -0300

    resolução type bool

---
+++ 04_ex_bool.py
@@ -0,0 +1,68 @@
+## Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# print("\nEsse programa receberá a idade de duas pessoas digitadas pelo usuário e devolverá se elas podem ou não dirigir.\n")
+# print("Vamos começar\n")
+
+# nome1 = str(input("Digite o nome da primeira pessoa: \n"))
+# idade_pessoa1 = int(input(f"Digite a idade do(a) {nome1}: \n"))
+
+# nome2 = str(input("\nDigite o nome da segunda pessoa\n"))
+# idade_pessoa2 = int(input("Digite a idade da segunda pessoa: \n"))
+
+
+# if idade_pessoa1 >=18 and idade_pessoa2 >=18:
+#     print(f"\n {nome1} e {nome2} tem mais de 18 anos e podem dirigir\n")
+# else:
+#     print("Pelo menos uma das duas pessoas tem menos de 18 anos e não pode dirigir\n")
+
+
+
+## Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# print("\nEsse programa receberá a idade de duas pessoas digitadas pelo usuário e devolverá se elas podem ou não dirigir.\n")
+# print("Vamos começar\n")
+
+# nome1 = str(input("Digite o nome da primeira pessoa: \n"))
+# idade_pessoa1 = int(input(f"Digite a idade do(a) {nome1}: \n"))
+
+# nome2 = str(input("\nDigite o nome da segunda pessoa\n"))
+# idade_pessoa2 = int(input("Digite a idade da segunda pessoa: \n"))
+
+
+# if idade_pessoa1 >=18 or idade_pessoa2 >=18:
+#     print("Pelo menos uma das duas pessoas é maior de 18 anos e pode dirigir\n")
+# else:
+#     print(f"\n {nome1} e {nome2} são menores de 18 anos e não podem dirigir\n")
+
+
+
+## Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+# a = bool(input("\nDigite se a sua variavél é True ou False: \n"))
+
+# if a:
+
+# print("Pelo menos uma das duas pessoas é maior de 18 anos e pode dirigir\n")
+
+# ----------------------------
+
+# valor1 = input("\nDigite um valor: \n")
+
+# resultado_not = not valor1
+# print(f"Resultado do NOT lógico: {resultado_not}")
+
+
+
+# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+# num1 = 5
+# num2 = 5
+# resultado_igualdade = (num1 == num2)
+# print("Resultado da igualdade:", resultado_igualdade)
+
+
+
+## Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# resultado_diferenca = (num1 != num2)
+# print("Resultado da diferença:", resultado_diferenca)
