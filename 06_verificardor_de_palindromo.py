1a365c4f261c9c79edc6d84479311560ceb7ef80
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-03-28 20:49:52 -0300

    adc mais exercícios - TypeError, Type Check, Type Conversion, try-except e if

---
+++ 06_verificardor_de_palindromo.py
@@ -0,0 +1,3 @@
+# Exercício 22: Verificador de Palíndromo
+# Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+
