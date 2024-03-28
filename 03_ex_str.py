57aa0b0bf60761db99249decf7cd295fc083bda2
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-03-28 20:38:54 -0300

    resolução type str

---
+++ 03_ex_str.py
@@ -0,0 +1,58 @@
+## Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+# print("\nEsse programa transforma uma string para que todos os caracteres sejam minúsculos\n")
+# print("Vamos começar\n")
+
+# word1 = str(input("\nInsira um palavra ou texto qualquer: \n")).upper()
+
+# print(f"\n{word1}")
+
+
+
+## Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+# print("\nEsse programa receberá um nome digitado pelo usuário e transformará todos os caracteres para letras minúsculas\n")
+# print("Vamos começar\n")
+
+# nome = str(input("Digite seu nome completo: \n")).lower()
+
+# print(f"\nO nome digitado foi {nome}.")
+
+
+
+## Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+# print("\nEsse programa receberá uma frase digitada pelo usuário e se houver espaços no começo ou final da frase o programa retira os espaços automaticamente\n")
+# print("Vamos começar\n")
+
+# frase1 = str(input("\nDigite uma frase que você goste: \n")).strip()
+
+# print("\nFrase sem espaços no início e no final: {}".format(frase1))
+
+
+
+## Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+# print("\nEsse programa receberá uma data digitada pelo usuário e devolverá o dia, mês e ano separados automaticamente\n")
+# print("Vamos começar\n")
+
+# data = str(input("Digite uma data à sua escolha, sendo o formato dd/mm/aaaa: \n"))
+# lista = data.split("/")
+
+# print(f"\nO dia digitado foi {lista[0]}.")
+# print(f"\nO mês digitado foi {lista[1]}.")
+# print(f"\nO ano digitado foi {lista[2]}.")
+
+
+
+## Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# print("\nEsse programa receberá uma duas palavras/frases digitada separadamente pelo usuário e devolverá uma concatenação das strings digitadas.\n")
+# print("Vamos começar\n")
+
+# palavras1 = input("Digite um palavra ou frase: \n")
+# palavras2 = input("Digite outra palavra ou frase que será concatenada com a de cima: \n")
+
+# juntar = palavras1 + " " + palavras2
+
+# print(f"Juntando seus dois input acima formou-se o texto: {juntar}")
